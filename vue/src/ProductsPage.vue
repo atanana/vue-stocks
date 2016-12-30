67e76c757af75feafa420710c3ac1bdc20f31d00
Andrei Tanana <atanana@ampsoft.by>
2016-12-30 14:44:29 +0300

    load products

---
+++ vue/src/ProductsPage.vue
@@ -0,0 +1,35 @@
+<template>
+  <ProductsList
+    :products="products"
+  />
+</template>
+
+<script>
+
+  import axios from 'axios'
+  import ProductsList from 'components/ProductsList'
+
+  export default {
+    name: 'app',
+    components: {
+      ProductsList
+    },
+    data() {
+      return {
+        products: []
+      }
+    },
+    created () {
+      axios.get('/api/products/all')
+        .then((response) => {
+          this.products = response.data;
+        });
+    }
+  }
+</script>
+
+<style>
+  #app {
+    margin: 1em;
+  }
+</style>
--- vue/src/main.js
+++ vue/src/main.js
@@ -2,19 +2,13 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import App from './App'
-import axios from 'axios'
+import ProductsPage from './ProductsPage'
 
 Vue.use(VueRouter);
 
 const Products = {
-  template: '<App :products="products"/>',
-  components: {App},
-  data () {
-    return {
-      products: []
-    }
-  }
+  template: '<ProductsPage/>',
+  components: {ProductsPage}
 };
 const Categories = {template: '<div>bar</div>'};
 
@@ -30,8 +24,3 @@ const router = new VueRouter({
 const app = new Vue({
   router
 }).$mount('#app');
-
-// axios.get('/api/products/all')
-//   .then((response) => {
-//     Products.products = response.data;
-//   });
