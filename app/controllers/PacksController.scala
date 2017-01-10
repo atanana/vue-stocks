0c6da16129038619cd344c83c7ce90aa2daf5a2c
atanana <tanana@email.ru>
2017-01-10 23:32:59 +0300

    packs controller

---
+++ app/controllers/PacksController.scala
@@ -0,0 +1,64 @@
+package controllers
+
+import javax.inject.Inject
+
+import models.db._
+import play.api.Logger
+import play.api.libs.functional.syntax._
+import play.api.libs.json.{Json, Writes, _}
+import play.api.mvc._
+import services.db.DBService
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import scala.util.{Failure, Success, Try}
+
+//todo refactor with categories controller
+class PacksController @Inject()(val db: DBService) extends Controller with Tables {
+  private implicit val table = packs
+
+  case class ClientPack(id: Option[Int], name: String)
+
+  implicit val packWrites: Writes[Pack] = (
+    (JsPath \ "id").write[Int] and
+      (JsPath \ "name").write[String]
+    ) (unlift(Pack.unapply))
+
+  implicit val clientCategoryReads: Reads[ClientPack] = (
+    (JsPath \ "id").readNullable[Int] and
+      (JsPath \ "name").read[String]
+    ) (ClientPack.apply _)
+
+  def allPacks: Action[AnyContent] = Action.async {
+    allSorted
+  }
+
+  private def allSorted = {
+    sorted[Packs, Pack].map(categories => {
+      Ok(Json.toJson(categories))
+    })
+  }
+
+  //noinspection MutatorLikeMethodIsParameterless
+  def updatePacks: Action[JsValue] = Action.async(parse.json) { request =>
+    Try(
+      request.body.as[JsArray].value
+        .map(_.as[ClientPack])
+    ) match {
+      case Success(newPacks) =>
+        val newPacksIds = newPacks.map(_.id).filter(_.isDefined).map(_.get)
+        Future.sequence(
+          deleteBesides(newPacksIds) +: updateAndCreatePacks(newPacks)
+        ).flatMap(_ => allSorted)
+      case Failure(exception) =>
+        Logger.error(s"Cannot parse request: ${request.body}", exception)
+        Future(BadRequest)
+    }
+  }
+
+  private def updateAndCreatePacks(newCategories: Seq[ClientPack]) = {
+    newCategories.map(category => category.id
+      .map(id => updateName(id, category.name))
+      .getOrElse(addCategory(category.name)))
+  }
+}
