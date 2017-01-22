d29dbe5f21778b17117b4f94c6ba4eba1a0790ee
atanana <tanana@email.ru>
2017-01-22 16:46:59 +0300

    routing improvements

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,6 +5,9 @@ import javax.inject.Inject
 import play.api.mvc._
 
 class Application @Inject()(val authorizedAction: AuthorizedAction) extends Controller {
+  def index2: Action[AnyContent] = authorizedAction {
+    Ok(views.html.index())
+  }
 
   def index(path: String): Action[AnyContent] = authorizedAction {
     Ok(views.html.index())
