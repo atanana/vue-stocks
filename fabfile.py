be95eca8d16c21ec4d3247fce39f71d79abc044a
atanana <tanana@email.ru>
2017-01-26 23:25:29 +0300

    deploy script

---
+++ fabfile.py
@@ -0,0 +1,14 @@
+from fabric.api import run, sudo, local
+from fabric.operations import put
+from fabric.context_managers import cd, lcd
+
+def deploy():
+    with lcd('vue'):
+        local('npm run build')
+    local('activator clean dist')
+    with cd('vue-stocks'):
+        sudo('systemctl stop vue-stocks')
+        run('rm -rf vue_stocks-1.0*')
+        put('target/universal/vue_stocks-1.0.zip', 'vue_stocks-1.0.zip')
+        run('unzip vue_stocks-1.0.zip')
+        sudo('systemctl start vue-stocks')
