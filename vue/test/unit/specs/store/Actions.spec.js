a0b7d5546bee0601279a33ded70b14042aae3dff
atanana <tanana@email.ru>
2017-03-03 22:17:38 +0300

    actions unit tests

--- vue/test/unit/specs/store/Actions.spec.js
+++ vue/test/unit/specs/store/Actions.spec.js
@@ -25,6 +25,58 @@ describe('actions.js', () => {
     checkLoadedOnce('/api/product-types/all', actions.loadProductTypes, 'setProductTypes', done);
   });
 
+  it('should update categories', done => {
+    checkUpdate('/api/categories/update', actions.updateCategories, 'setCategories', done);
+  });
+
+  it('should update packs', done => {
+    checkUpdate('/api/packs/update', actions.updatePacks, 'setPacks', done);
+  });
+
+  it('should update product types', done => {
+    checkUpdate('/api/product-types/update', actions.updateProductTypes, 'setProductTypes', done);
+  });
+
+  it('should add product', done => {
+    checkPost('/api/products/new', actions.addProduct, 'setProducts', done);
+  });
+
+  it('should delete product', done => {
+    checkPost('/api/products/delete', actions.deleteProduct, 'setProducts', done);
+  });
+
+  function checkUpdate(path, method, action, done) {
+    checkMethod(path, method, action, done, server.onPut);
+  }
+
+  function checkPost(path, method, action, done) {
+    checkMethod(path, method, action, done, server.onPost);
+  }
+
+  function checkMethod(path, method, action, done, serverMethod) {
+    const data = {
+      items: [
+        {name: 'item 1'},
+        {name: 'item 2'},
+        {name: 'item 3'}
+      ]
+    };
+    const newData = {
+      items: [
+        {name: 'item 1'}
+      ]
+    };
+    serverMethod.call(server, path, newData).reply(200, data);
+    const spy = sinon.spy();
+
+    method.call(actions, {commit: spy}, newData);
+
+    setTimeout(() => {
+      expect(spy).to.calledWith(action, data);
+      done();
+    }, 0);
+  }
+
   function checkLoadedOnce(path, method, action, done) {
     const data = {
       items: [
